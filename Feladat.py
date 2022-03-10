5bae30208492ebed3ede5c0b28471760c0b5c567
tpeterb <peter.balazs.toth@gmail.com>
2022-03-10 15:27:36 +0100

    Create Feladat.py

---
+++ Feladat.py
@@ -0,0 +1,11 @@
+class Feladat:
+
+    def __init__(self, kezdo, cel = None):
+        self.kezdo = kezdo
+        self.cel = cel
+
+    def celteszt(self, allapot):
+        raise NotImplementedError
+
+    def rakovetkezo(self, allapot):
+        raise NotImplementedError
